3c352b4ae239d80e072c1556853931e37a6f7b92
Maurits Wilke <mauritswilke@gmail.com>
2025-02-01 00:02:55 +0100

    feat: add https stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const decoder = new TextDecoder("utf-8")
+const keyFile = Deno.readFileSync(`${__dirname}/cert/key.pem`);
+const certFile = Deno.readFileSync(`${__dirname}/cert/cert.pem`);
+
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    server: {
+        https: {
+            key: decoder.decode(keyFile),
+            cert: decoder.decode(certFile),
+        },
+        proxy: {}
+    }
 });
